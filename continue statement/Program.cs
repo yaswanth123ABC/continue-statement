3e34a57685da620e6ee2046d94ebedd008c64199
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-10 13:36:18 +0530

    Add project files.

---
+++ continue statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                if (i == 3)
+                    continue;
+                Console.WriteLine("i value: {0}", i);
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
